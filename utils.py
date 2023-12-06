f753f8221fa5a78a353526b5bf66143d910a3161
3096 <7045393+3096@users.noreply.github.com>
2023-12-05 20:59:25 -0800

    feat: switch to ipynb

--- utils.py
+++ utils.py
@@ -9,5 +9,5 @@ def process_dir(dir_path, ext, output_path, output_ext, algorithm):
     """
     for filename in os.listdir(dir_path):
         if filename.lower().endswith(ext):
-            cv2.imwrite(os.path.join(output_path, os.path.basename(filename) + output_ext),
+            cv2.imwrite(os.path.join(output_path, os.path.splitext(filename)[0] + output_ext),
                         algorithm(cv2.imread(os.path.join(dir_path + filename))))
